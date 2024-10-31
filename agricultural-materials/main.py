64df24addc0b34c58a81c8b3c0a78fb22be7bd79
Alexandre Gomez Martins <alexandregomezm@gmail.com>
2024-10-31 16:55:12 -0300

    now using jupiter extension

--- agricultural-materials/main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-# importando o conjunto de dados usando dataframe
-df = pd.read_csv("python-dataanalysis\agricultural-materials")
-
-# explorando o conjunto de dados
-df.sample(10)
